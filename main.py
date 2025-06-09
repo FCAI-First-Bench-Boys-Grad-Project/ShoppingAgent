c38437fc22f0b5578f670586f30efab3b23f7c7e
SuperNova74-k <kingkhaled.w@gmail.com>
2025-06-09 04:16:03 +0300

    fix async issues

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from src.agents.manager import manager_agent
 from src.agents.product_hunter import product_hunter_agnet
 from llama_index.core.agent.workflow import AgentWorkflow, ReActAgent, FunctionAgent
 from llama_index.core.workflow import Context
+import asyncio
 
-
-def main():
+async def main():
 
     workflow = AgentWorkflow(
         agents=[manager_agent, product_hunter_agnet],
@@ -14,7 +14,7 @@ def main():
     # To keep memory
     ctx = Context(workflow)
 
-    workflow.run(
+    response = await workflow.run(
         user_msg="I want to build a Gaming PC for around 1000 Dollars, I don't care about looks or RGB but I care about performance, I want the greatest performance for gaming on 1080p with these 1000 dollars, also the pc should have at least 16 GBs of ram and 1TB ssd , I live in Texas. Can you please give me the pc parts links I should buy online to build that pc ?",
         ctx=ctx
     )
@@ -22,4 +22,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
