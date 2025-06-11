f9c3c639a53b2c1b900b9438d01ea2c27ecd057a
Error message: AMFAM <abdulrahmanmfam2003@gmail.com>
2025-06-11 03:13:41 +0300

    add tag

--- src/agents/manager.py
+++
@@ -1,8 +0,0 @@
-class ManagerIdentity():
-    def __init__(self):
-        name="manager_agent"
-        description="The Main Agent Responsible of Fulfilling the user's request in the task of automated shopping."
-        system_prompt=\
-        '''
-        '''
-        tools=[]
--- todo.md
+++
@@ -1,6 +0,0 @@
-* Online Shopping and Must Return Links or say there is none
-* Model
-* Search Engine
-
-
-* Traces and Logs
